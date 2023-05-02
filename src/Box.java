0300e8877cec972400e747736103f8ad52f3d140
Leonid Melnichenko <melnichenko.leon@gmail.com>
2023-05-02 12:19:42 +0300

    refactor box class add constructor

--- src/Box.java
+++ src/Box.java
@@ -5,12 +5,19 @@ class Box {
     double height;
     double depth;
 
+    public Box() {
+        System.out.println("Конструирование объекта box");
+        width = 10;
+        height = 10;
+        depth = 10;
+    }
+
     double volume() {
         return width * height * depth;
     }
-    void setDim(double w, double h, double d){
-            width = w;
-            height = h;
-            depth = d;
-        }
+//    void setDim(double w, double h, double d){
+//            width = w;
+//            height = h;
+//            depth = d;
+//        }
     }
