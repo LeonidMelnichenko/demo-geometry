af462185ab4954af2954dcd45016bec62cb78968
Leonid Melnichenko <melnichenko.leon@gmail.com>
2023-05-02 12:22:53 +0300

    refactor boxdemo class use constructor

--- src/BoxDemo.java
+++ src/BoxDemo.java
@@ -1,14 +1,12 @@
 package src;
 
-class BoxDemo4 {
+class BoxDemo6 {
     public static void main(String[] args) {
         Box myBox1 = new Box();
         Box myBox2 = new Box();
         double vol;
-
-        myBox1.setDim(10, 20, 15);
-        myBox2.setDim(3, 6 , 9);
-
+//        myBox1.setDim(10, 20, 15);
+//        myBox2.setDim(3, 6 , 9);
         vol = myBox1.volume();
         System.out.println("Объем равен " + vol);
         vol = myBox2.volume();
