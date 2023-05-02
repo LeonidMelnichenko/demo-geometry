6fe81ce2f3ccc135d1a6aef98a51f5fd642a32c6
Leonid Melnichenko <melnichenko.leon@gmail.com>
2023-05-02 12:35:43 +0300

    refactor boxdemo class use constructor with params

--- src/BoxDemo.java
+++ src/BoxDemo.java
@@ -1,9 +1,9 @@
 package src;
 
-class BoxDemo6 {
+class BoxDemo7 {
     public static void main(String[] args) {
-        Box myBox1 = new Box();
-        Box myBox2 = new Box();
+        Box myBox1 = new Box(10, 20, 15);
+        Box myBox2 = new Box(3, 6, 9);
         double vol;
 //        myBox1.setDim(10, 20, 15);
 //        myBox2.setDim(3, 6 , 9);
